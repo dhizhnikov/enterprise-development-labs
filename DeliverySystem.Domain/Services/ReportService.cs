424629bad5f9aa4e735fd1ccbefb0dca22e51753
dhizhnikov <dima542542@gmail.com>
2025-06-19 13:26:49 +0400

    fix summary

--- DeliverySystem.Domain/Services/ReportService.cs
+++ DeliverySystem.Domain/Services/ReportService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using static ReportService;
 
 /// <summary>
-/// Сервис для генерации отчетов службы доставки
+/// Сервис для генерации отчетов службы доставки.
 /// </summary>
 public class ReportService : IReportService
 {
@@ -12,7 +12,7 @@ public class ReportService : IReportService
     private readonly ICourierRepository _courierRepository;
 
     /// <summary>
-    /// Инициализирует новый экземпляр ReportService
+    /// Инициализирует новый экземпляр класса ReportService.
     /// </summary>
     public ReportService(IOrderRepository orderRepository, ICourierRepository courierRepository)
     {
@@ -21,7 +21,7 @@ public class ReportService : IReportService
     }
 
     /// <summary>
-    /// Получить информацию о заказе по номеру (Запрос 1)
+    /// Получает детальную информацию о заказе по его идентификатору.
     /// </summary>
     public OrderResult GetOrderDetails(int orderId)
     {
@@ -33,15 +33,33 @@ public class ReportService : IReportService
     }
 
     /// <summary>
-    /// Модели результатов для отчетов
+    /// Представляет результат запроса информации о заказе.
     /// </summary>
     public class OrderResult
     {
+        /// <summary>
+        /// Идентификатор заказа
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Дата оформления заказа
+        /// </summary>
         public DateTime OrderDate { get; set; }
+
+        /// <summary>
+        /// Имя клиента, оформившего заказ
+        /// </summary>
         public string CustomerName { get; set; }
+
+        /// <summary>
+        /// Тип продукта в заказе
+        /// </summary>
         public ProductType ProductType { get; set; }
 
+        /// <summary>
+        /// Инициализирует новый экземпляр класса OrderResult на основе данных заказа.
+        /// </summary>
         public OrderResult(Order order)
         {
             Id = order.Id;
@@ -51,11 +69,24 @@ public class ReportService : IReportService
         }
     }
 
+    /// <summary>
+    /// Представляет результат запроса информации о курьере.
+    /// </summary>
     public class CourierResult
     {
+        /// <summary>
+        /// Идентификатор курьера
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Полное имя курьера
+        /// </summary>
         public string FullName { get; set; }
 
+        /// <summary>
+        /// Инициализирует новый экземпляр класса CourierResult на основе данных курьера.
+        /// </summary>
         public CourierResult(Courier courier)
         {
             Id = courier.Id;
@@ -63,34 +94,81 @@ public class ReportService : IReportService
         }
     }
 
+    /// <summary>
+    /// Представляет график доставки, связывающий заказ с назначенным курьером.
+    /// </summary>
     public class DeliverySchedule
     {
+        /// <summary>
+        /// Информация о заказе
+        /// </summary>
         public OrderResult Order { get; set; }
+
+        /// <summary>
+        /// Информация о курьере
+        /// </summary>
         public CourierResult Courier { get; set; }
     }
 
+    /// <summary>
+    /// Содержит статистику по типам продуктов.
+    /// </summary>
     public class ProductTypeStats
     {
+        /// <summary>
+        /// Количество крупногабаритных товаров
+        /// </summary>
         public int LargeItemsCount { get; set; }
+
+        /// <summary>
+        /// Количество мелкогабаритных товаров
+        /// </summary>
         public int SmallItemsCount { get; set; }
     }
 
+    /// <summary>
+    /// Содержит статистику по курьерам.
+    /// </summary>
     public class CourierStats
     {
+        /// <summary>
+        /// Информация о курьере
+        /// </summary>
         public CourierResult Courier { get; set; }
+
+        /// <summary>
+        /// Количество завершенных заказов
+        /// </summary>
         public int CompletedOrdersCount { get; set; }
     }
-
+    /// <summary>
+    /// Содержит информацию о задержках доставки.
+    /// </summary>
     public class DeliveryDelayInfo
     {
+        /// <summary>
+        /// Информация о заказе
+        /// </summary>
         public OrderResult Order { get; set; }
+
+        /// <summary>
+        /// Информация о курьере
+        /// </summary>
         public CourierResult Courier { get; set; }
+
+        /// <summary>
+        /// Планируемое время доставки
+        /// </summary>
         public DateTime PlannedDeliveryTime { get; set; }
+
+        /// <summary>
+        /// Фактическое время доставки
+        /// </summary>
         public DateTime ActualDeliveryTime { get; set; }
+
+        /// <summary>
+        /// Продолжительность задержки
+        /// </summary>
         public TimeSpan DelayDuration { get; set; }
     }
-}
-
-public interface IReportService
-{
 }
